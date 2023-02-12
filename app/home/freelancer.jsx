48422e61fac1e1dfe95875de1efeb1d7e02dc02b
mocilnikcarlos <mocilnik.web@gmail.com>
2023-02-12 11:49:55 -0300

    docs(Root): add function colors

--- app/home/freelancer.jsx
+++ app/home/freelancer.jsx
@@ -1,7 +1,13 @@
 import style from "./freelancer.module.scss";
 
 const Freelancer = () => {
-  return <h2 className={style.title}>Pagina de inicio</h2>;
+  return (
+    <>
+      <h2 className={style.title}>Pagina de inicio</h2>
+      <div className={style.card}></div>
+      <div className={style.card1}></div>
+    </>
+  );
 };
 
 export default Freelancer;
